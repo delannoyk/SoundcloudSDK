98b467569a43cf810185d6e396a0f0d0e1ef376f
Kevin Delannoy <delannoyk@gmail.com>
2015-06-20 14:34:33 +0200

    Added comment parsing

--- sources/SoundcloudSDK/model/Comment.swift
+++ sources/SoundcloudSDK/model/Comment.swift
@@ -19,19 +19,19 @@ public struct Comment {
     public let createdAt: NSDate
 
     ///Track's identifier
-    public let trackIdentifier: Int
+    public let trackIdentifier: Int?
 
     ///User's identifier
-    public let userIdentifier: Int
+    public let userIdentifier: Int?
 
     ///Comment's timestamp
-    public let timestamp: NSTimeInterval
+    public let timestamp: NSTimeInterval?
 
     ///Body
     public let body: String
 
     ///User
-    public let user: User
+    public let user: User?
 }
 
 ////////////////////////////////////////////////////////////////////////////
@@ -45,13 +45,18 @@ public struct Comment {
 
 internal extension Comment {
     init?(JSON: JSONObject) {
-        /*if let identifier = JSON["id"].intValue, user = User(JSON: JSON["user"]) {
-
+        if let identifier = JSON["id"].intValue, body = JSON["body"].stringValue {
+            self.identifier = identifier
+            self.createdAt = JSON["created_at"].dateValue("yyyy/MM/dd HH:mm:ss VVVV") ?? NSDate()
+            self.trackIdentifier = JSON["track_id"].intValue
+            self.userIdentifier = JSON["user_id"].intValue
+            self.timestamp = JSON["timestamp"].doubleValue
+            self.body = body
+            self.user = User(JSON: JSON["user"])
         }
         else {
             return nil
-        }*/
-        return nil
+        }
     }
 }
 
