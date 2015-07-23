703dd9d07a02becc957229a5972ed50744746702
Kevin Delannoy <delannoyk@gmail.com>
2015-07-23 19:34:31 +0200

    Added tests for URL Encoding method

--- sources/SoundcloudSDKTests/SoundcloudSDKTests.swift
+++ sources/SoundcloudSDKTests/SoundcloudSDKTests.swift
@@ -62,6 +62,28 @@ class SoundcloudSDKTests: XCTestCase {
     ////////////////////////////////////////////////////////////////////////////
 
 
+    // MARK: URL Encode
+    ////////////////////////////////////////////////////////////////////////////
+
+    func testURLEncoding() {
+        let parameters = [
+            "1": "1",
+            "2": "2 2",
+            "3": "3 3 3",
+            "4": "4@",
+            "5": "%@ & test ="
+        ]
+
+        XCTAssert(parameters["1"]!.URLEncodedValue == "1", "1 should be transformed to 1")
+        XCTAssert(parameters["2"]!.URLEncodedValue == "2%202", "2 2 should be transformed to 2%202")
+        XCTAssert(parameters["3"]!.URLEncodedValue == "3%203%203", "3 3 3 should be transformed to 3%203%203")
+        XCTAssert(parameters["4"]!.URLEncodedValue == "4%40", "4@ should be transformed to 4%40")
+        XCTAssert(parameters["5"]!.URLEncodedValue == "%25%40%20%26%20test%20%3D", "%@ should be transformed to %25%40%20%26%20test%20%3D")
+    }
+
+    ////////////////////////////////////////////////////////////////////////////
+
+
     // MARK: Caching tests
     ////////////////////////////////////////////////////////////////////////////
 
