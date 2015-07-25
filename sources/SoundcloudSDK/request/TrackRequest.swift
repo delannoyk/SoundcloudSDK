3f22fe65bb3dc0445cbadedcdcc3850ec38ff795
Kevin Delannoy <delannoyk@gmail.com>
2015-07-25 18:51:38 +0200

    Added a way to comment a track

--- sources/SoundcloudSDK/request/TrackRequest.swift
+++ sources/SoundcloudSDK/request/TrackRequest.swift
@@ -73,6 +73,37 @@ public extension Track {
     }
 
     /**
+    Create a new comment on a track
+    
+    **This method requires a Session.**
+
+    :param: body       The text body of the comment
+    :param: timestamp  The progression of the track when the comment was validated
+    :param: completion The closure that will be called when the comment is posted or upon error
+    */
+    public func comment(body: String, timestamp: NSTimeInterval, completion: Result<Comment> -> Void) {
+        if let oauthToken = Soundcloud.session?.accessToken {
+            let URL = Track.BaseURL.URLByAppendingPathComponent("\(identifier)/comments.json")
+            let parameters = ["client_id": Soundcloud.clientIdentifier!,
+                "comment[body]": body,
+                "comment[timestamp]": "\(timestamp)",
+                "oauth_token": oauthToken
+            ]
+
+            let request = Request(URL: URL, method: .POST, parameters: parameters, parse: {
+                if let comments = Comment(JSON: $0) {
+                    return .Success(Box(comments))
+                }
+                return .Failure(GenericError)
+            }, completion: completion)
+            request.start()
+        }
+        else {
+            completion(.Failure(GenericError))
+        }
+    }
+
+    /**
     Fetch the list of users that favorited the track.
 
     :param: completion The closure that will be called when users are loaded or upon error
