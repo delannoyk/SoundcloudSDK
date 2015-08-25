4fce8264bb174811fb1e212361bf41dcbb245252
Kevin Delannoy <delannoyk@gmail.com>
2015-08-25 18:47:55 +0200

    Added a way to fetch a playlist

---
+++ sources/SoundcloudSDK/request/PlaylistRequest.swift
@@ -0,0 +1,37 @@
+//
+//  PlaylistRequest.swift
+//  
+//
+//  Created by Kevin DELANNOY on 06/08/15.
+//
+//
+
+import UIKit
+
+public extension Playlist {
+    internal static let BaseURL = NSURL(string: "https://api.soundcloud.com/playlist")!
+
+    /**
+    Load playlist with a specific identifier
+
+    :param: identifier  The identifier of the playlist to load
+    :param: secretToken The secret token to access the playlist or nil
+    :param: completion  The closure that will be called when playlist is loaded or upon error
+    */
+    public static func playlist(identifier: Int, secretToken: String? = nil, completion: Result<Playlist> -> Void) {
+        var URL = BaseURL.URLByAppendingPathComponent("\(identifier)")
+        if let secretToken = secretToken {
+            URL = URL.URLByAppendingPathComponent(secretToken)
+        }
+
+        let parameters = ["client_id": Soundcloud.clientIdentifier!]
+
+        let request = Request(URL: URL, method: .GET, parameters: parameters, parse: {
+            if let playlist = Playlist(JSON: $0) {
+                return .Success(Box(playlist))
+            }
+            return .Failure(GenericError)
+        }, completion: completion)
+        request.start()
+    }
+}
