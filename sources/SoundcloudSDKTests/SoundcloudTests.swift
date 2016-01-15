0daf4aafa9e4ad54b0028d48318c63cdcd9550a7
Kevin Delannoy <delannoyk@gmail.com>
2016-01-15 21:04:20 +0100

    Began to write more tests

---
+++ sources/SoundcloudSDKTests/SoundcloudTests.swift
@@ -0,0 +1,29 @@
+//
+//  SoundcloudTests.swift
+//  SoundcloudTests
+//
+//  Created by Kevin DELANNOY on 24/02/15.
+//  Copyright (c) 2015 Kevin Delannoy. All rights reserved.
+//
+
+import XCTest
+@testable import Soundcloud
+
+class SoundcloudTests: XCTestCase {
+    func testCredentialsError() {
+        let resolveExpectation = expectationWithDescription("Waiting for `resolve` response")
+        Soundcloud.resolve("") { response in
+            XCTAssertFalse(response.response.isSuccessful, "The result should be a .Failure")
+            XCTAssert(response.response.error! == SoundcloudError.CredentialsNotSet, "Errors should be about Credentials")
+            resolveExpectation.fulfill()
+        }
+
+
+
+        waitForExpectationsWithTimeout(1) { e in
+            if let _ = e {
+                XCTFail("Expectations should have been fulfilled immediatly")
+            }
+        }
+    }
+}
