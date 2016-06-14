7555ad4e3d592af1994571551ac99ca82fe72a6e
Kevin Delannoy <delannoyk@gmail.com>
2016-06-14 08:26:07 +0200

    Added a way to create a playlist

--- sources/SoundcloudSDK/request/PlaylistRequest.swift
+++ sources/SoundcloudSDK/request/PlaylistRequest.swift
@@ -41,4 +41,39 @@ public extension Playlist {
         })
         request.start()
     }
+
+    /**
+     Creates a playlist with a name and a specific sharing access
+
+     - parameter name:          The name of the playlist
+     - parameter sharingAccess: The required sharing access
+     - parameter completion:    The closure that will be called when playlist is created or upon error
+     */
+    public static func createWithName(name: String, sharingAccess: SharingAccess, completion: SimpleAPIResponse<Playlist> -> Void) {
+        guard let clientIdentifier = Soundcloud.clientIdentifier else {
+            completion(SimpleAPIResponse(.CredentialsNotSet))
+            return
+        }
+
+        guard let oauthToken = Soundcloud.session?.accessToken else {
+            completion(SimpleAPIResponse(.NeedsLogin))
+            return
+        }
+
+        let queryStringParameters = ["client_id": clientIdentifier, "oauth_token": oauthToken]
+        let URL = BaseURL.URLByAppendingQueryString(queryStringParameters.queryString)
+
+        let parameters = ["playlist[title]": name,
+            "playlist[sharing]": sharingAccess.rawValue]
+
+        let request = Request(URL: URL, method: .POST, parameters: parameters, parse: {
+            if let playlist = Playlist(JSON: $0) {
+                return .Success(playlist)
+            }
+            return .Failure(.Parsing)
+        }) { result in
+            completion(SimpleAPIResponse(result))
+        }
+        request.start()
+    }
 }
